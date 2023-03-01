978118e3cdbb7383c4e0bb29352a3006c1437f71
mattmcclaskey <matt@kasmweb.com>
2023-03-01 16:03:00 -0500

    check clipboard on keypress

--- core/rfb.js
+++ core/rfb.js
@@ -1570,6 +1570,11 @@ export default class RFB extends EventTargetMixin {
 
     _handleKeyEvent(keysym, code, down) {
         this.sendKey(keysym, code, down);
+
+        if (this._resendClipboardNextUserDrivenEvent) {
+            this.checkLocalClipboard();
+        }
+
     }
 
     _handleMouse(ev) {
